3d140cdd146eb60abe7a618a07303a65975842ea
lucianoserafim <lucianoserafimdesouza@gmail>
2016-05-20 16:50:06 -0300

    Commit ajuste função de geração de aleatórios.

--- R/rtanb.R
+++ R/rtanb.R
@@ -1,25 +1,27 @@
-# rtanb
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
+#' Generates random deviates from a TanBullXII probability distribution.
+#'
+#' @param n Number of observations to be generated.
+#' @param c C parameter.
+#' @param k K parameter.
+#' @param s S parameter.
+#' @return A vector with n observations of the TanBullXII distribution.
+#' @examples
+#' rtanb(1, 3, 2, 2)
+#' rtanb(1, 0.3, 0.1, 0.8)
 
 rtanb<-function(n,c,k,s){
   library(pracma)
-  y=(atan(0.999999999))
-  limsup = (s*(((1-((4/pi)*y))^(-(1/k)))-1)^(1/c))
+  library(fdrtool)
+
   accept = c()
   count = 0
+
   while (length(accept) < n){
-    U = runif(1,0,limsup)
-    x = runif(1,0,limsup)
-    if(dunif(x, 0, limsup)*U <= dtanb(x,c,k,s)) {
+
+    U <- rhalfnorm(1)
+    x <- rhalfnorm(1)
+
+    if(U <= dtanb(x,c,k,s)/(sqrt(pi)*dhalfnorm(x)/sqrt(2))) {
       accept[count] = x
       count = count + 1
     }
